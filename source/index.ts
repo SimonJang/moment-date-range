705c40b6e3eaf54daa62d997687d1d5a4621373a
Simon Jang <simon.jang.contact@gmail.com>
2020-01-26 21:05:29 +0100

    Basic module implementation

--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,83 @@
-export function foo(): void {
-	// Create magic
+import {Moment, unitOfTime} from 'moment';
+import * as moment from 'moment';
+
+interface RangeOptions {
+	step: number;
+	unit: string;
+}
+
+interface IteratorOptions extends RangeOptions {
+	start: Moment;
+	end?: Moment;
+}
+
+function isMoment(value: Partial<RangeOptions> | Moment | undefined): value is Moment {
+	if (!value) {
+		return false;
+	}
+
+	return (value as any).format
+}
+
+function assert(condition: boolean, message: string): asserts condition {
+	if (!condition) {
+		throw Error(message);
+	}
+}
+
+function* dateIterator(options: IteratorOptions): Generator<Moment> {
+	const {start, end, step, unit} = options;
+
+	let rangeStart = moment(start);
+
+	// TODO: Work with moving backwards
+	while (!end || rangeStart.isSameOrBefore(end)) {
+		yield moment(rangeStart);
+
+		// Check what happens when adding negative numbers
+		rangeStart = moment(rangeStart).add(step, unit as unitOfTime.DurationConstructor);
+	}
+}
+
+export function range(start: Moment, rangeOptions?: Partial<RangeOptions>): Generator;
+export function range(start: Moment, end: Moment, rangeOptions?: Partial<RangeOptions>): Generator;
+export function range(
+	start: Moment,
+	end?: Moment | Partial<RangeOptions>,
+	rangeOptions?: Partial<RangeOptions>
+): Generator {
+	let rangeEnd: Moment | undefined;
+	let opts: Partial<RangeOptions> = rangeOptions ?? {};
+
+	if (isMoment(end)) {
+		rangeEnd = end;
+	} else {
+		opts = end ?? {}
+	}
+
+	const options = {
+		step: 1,
+		unit: 'd',
+		...opts,
+	}
+
+	assert(options.step !== 0, '`step` cannot be `0`');
+	assert(Number.isInteger(options.step), '`step` must be an integer');
+
+	if (rangeEnd) {
+		assert(
+			options.step > 0 && start.isAfter(rangeEnd),
+			'`start` needs to be before `end`'
+		);
+		assert(
+			options.step < 0 && start.isBefore(rangeEnd),
+			'`end` needs to be before `after`'
+		);
+	}
+
+	return dateIterator({
+		...options,
+		start,
+		end: rangeEnd
+	});
 }
--- source/test/test.ts
+++ source/test/test.ts
@@ -1,6 +1,35 @@
 import test from 'ava';
+import * as moment from 'moment';
+import {range} from '..';
 
-test('Hello test', t => {
-	// Write sensible test
-	t.true(true);
+test('should fail on validation', t => {
+	t.throws(() => range(moment('2020-01-01'), moment('2019-12-31')), '`end` needs to be before `after`');
+	t.throws(() => range(moment('2020-01-01'), moment('2019-12-31'), {step: -1}), '`start` needs to be before `end`');
+	t.throws(() => range(moment('2020-01-01'), moment('2020-01-31'), {step: 0}), '`step` cannot be `0`');
+});
+
+test('should iterate forward with the default options', t => {
+	const iterator = range(moment('2020-01-01'));
+
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-01');
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-02');
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-03');
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-04');
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-05');
+});
+
+test('should iterate forward with a defined step', t => {
+	const iterator = range(moment('2020-01-01'), {step: 2});
+
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-01');
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-03');
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-05');
+});
+
+test('should iterate forward with a defined step and unit', t => {
+	const iterator = range(moment('2020-01-01'), {step: 1, unit: 'w'});
+
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-01');
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-08');
+	t.is(iterator.next().value.format('YYYY-MM-DD'), '2020-01-15');
 });
